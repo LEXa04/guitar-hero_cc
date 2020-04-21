a9390d1b48af02df8a6a454424ff6594ae323e5a
LEXa04 <LEXa04@users.noreply.github.com>
2020-04-21 17:01:45 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -56,8 +56,51 @@ function love.uptdate(dt)
     key.scrolling()
     memorycleaner()
 end
-
-function love.draw()
-    love.graphics.setColor(0, 0.66, 0,66, 1)
-    love.graphics.rectangle("fill", 300, 300, 50, 30)
+function love.load()
+    core["score"] =0
+    love.keyboard.setKeyRepeat(false)
 end
+function love.draw()
+    love.graphics.print("Score", 420, 150, 0, 4, 4)
+    love.graphics.print(core.score, 420, 200, 0, 4, 4)
+end
+
+function key.checkclicked(x)
+    for i = 0, core.touch do
+        if (buf[i]~= nil and buf[i].x == x and buf[i].y > 500 and buf[i].y < 550) then
+            buf[i].y = nil
+            buf[i].x = nil
+            buf[i]= nil
+            return (1)
+        end
+    end
+    return(0)
+end
+function love.keypressed(mykey)
+    if mykey == "q" and  key.checkclicked(100) == 1 then 
+        core.score = core.score + 10
+    end
+
+    if mykey =="s" and key.checkclicked(175) == 1 then
+        core.score = core.score + 10 
+    end
+    if mykey == "d" and key.checkclicked(250) == 1 then
+        core.score = core.score + 10 
+    end
+
+    if mykey == "f" and key.checkclicked(325) == 1 then 
+        core.score = core.score + 10
+    end 
+end
+
+function love.uptdate(dt)
+    dt=math.min(dt, 1/60)
+end
+function love.load()
+    core["logo"] = love.graphics.newImage("300px-Galekid_de_Pierre_Rochard.png")
+end
+
+-- function love.draw()
+--     love.graphics.setColor(0, 0.66, 0,66, 1)
+--     love.graphics.rectangle("fill", 300, 300, 50, 30)
+-- end
